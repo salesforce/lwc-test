b2424ed9f7e9412a7875d178e0fbd3dce82e0b69
Caridy Patiño <caridy@gmail.com>
2019-06-11 18:52:18 -0400

    refactor: starting lwc 1.1.x, jest tests will run native shadow

--- packages/@lwc/jest-preset/jest-preset.js
+++ packages/@lwc/jest-preset/jest-preset.js
@@ -4,8 +4,6 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
-const path = require('path');
-
 module.exports = {
     moduleFileExtensions: ['js', 'html'],
     moduleNameMapper: {
@@ -15,7 +13,7 @@ module.exports = {
         '^aura-storage$': require.resolve('./src/stubs/auraStorage.js'),
         '^lwc-test-utils$': require.resolve('@lwc/test-utils'),
     },
-    testEnvironment: path.resolve(__dirname, 'src/environment.js'),
+    testEnvironment: "jest-environment-jsdom-fifteen",
     resolver: require.resolve('@lwc/jest-resolver'),
     transform: {
         '^.+\\.(js|html|css)$': require.resolve('@lwc/jest-transformer'),

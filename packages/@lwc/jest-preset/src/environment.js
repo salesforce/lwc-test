1b3de15631a4e7be5c557e39afa24bdd7864029a
Trevor <tbliss@salesforce.com>
2019-09-26 14:04:52 -0700

    Revert "refactor: starting lwc 1.1.x, jest tests will run native shadow (#17)"
    This reverts commit d5a65fe372ca9682b375ae5bcd7d101e6a5cbb05.

---
+++ packages/@lwc/jest-preset/src/environment.js
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+
+const fs = require('fs');
+const vm = require('vm');
+const JSDOMEnvironment = require('jest-environment-jsdom-fifteen');
+const polyfillFile = require.resolve('@lwc/synthetic-shadow');
+const polyfillSource = fs.readFileSync(polyfillFile, 'utf-8');
+const polyfillScript = vm.createScript(polyfillSource, '@lwc/synthetic-shadow');
+
+let ShadowRootPatched;
+let HTMLSlotElementPatched;
+/** Synthetic Shadow Polyfill should be installed only once per worker */
+function _syntheticShadow(env) {
+    if (env.dom) {
+        env.dom.runVMScript(polyfillScript);
+        // eslint-disable-next-line no-func-assign
+        _syntheticShadow = function _syntheticShadow() {
+            /* noop */
+        };
+        // Cache the patched types and reuse
+        ShadowRootPatched = env.dom.window.ShadowRoot;
+        HTMLSlotElementPatched = env.dom.window.HTMLSlotElement;
+    }
+}
+
+module.exports = class JSDOMEnvironmentWithSyntheticShadow extends JSDOMEnvironment {
+    constructor(config, options = {}) {
+        super(config, options);
+
+        // Running the synthetic-shadow polyfill
+        _syntheticShadow(this);
+
+        // Instrinsics that should be transferred to each environment
+        this.global.ShadowRoot = ShadowRootPatched;
+        this.global.HTMLSlotElement = HTMLSlotElementPatched;
+    }
+};

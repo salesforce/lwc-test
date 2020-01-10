7c45b5e029a92fe5050c2662189c5057c18da80f
Eugene Kashida <ekashida@gmail.com>
2020-01-09 16:12:57 -0800

    build: fix incorrect dependencies (#58)
    * build: fix dependency versions
    
    * fix: correctly configure call to resolveModules
    
    * chore: remove engine version from cache key
    
    * build: add module-resolver dev dep
    
    * build: add module-resolver peer dep
    
    * chore: remove module-resolver usage
    
    * chore: add comment back

--- packages/@lwc/jest-transformer/src/index.js
+++ packages/@lwc/jest-transformer/src/index.js
@@ -6,7 +6,6 @@
  */
 const path = require('path');
 const crypto = require('crypto');
-const { getVersion } = require('lwc');
 
 const babelCore = require('@babel/core');
 const lwcCompiler = require('@lwc/compiler');
@@ -14,7 +13,6 @@ const jestPreset = require('babel-preset-jest');
 const babelCommonJs = require('@babel/plugin-transform-modules-commonjs');
 const babelDynamicImport = require('babel-plugin-transform-dynamic-import');
 
-const engineVersion = getVersion();
 const compilerVersion = require('@lwc/compiler/package.json').version;
 
 const apexScopedImport = require('./transforms/apex-scoped-import');
@@ -75,7 +73,7 @@ module.exports = {
             .createHash('md5')
             .update(JSON.stringify(options), 'utf8')
             .update(
-                fileData + filePath + configStr + NODE_ENV + compilerVersion + engineVersion,
+                fileData + filePath + configStr + NODE_ENV + compilerVersion,
                 'utf8'
             )
             .digest('hex');

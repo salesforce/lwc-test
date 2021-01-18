d396de780cbadc4f774d201c34359dec1d1fa260
Pierre-Marie Dartus <p.dartus@salesforce.com>
2021-01-18 10:37:35 +0100

    chore: simplify jest config

--- jest.config.js
+++ jest.config.js
@@ -4,16 +4,14 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
-
 module.exports = {
-    testMatch: ['<rootDir>/**/__tests__/*.spec.(js|ts)'],
+    projects: [
+        {
+            displayName: 'unit',
+            rootDir: '<rootDir>/packages',
 
-    // Global mono-repo code coverage threshold.
-    coverageThreshold: {
-        global: {
-            branches: 80,
-            functions: 85,
-            lines: 85,
+            testMatch: ['**/__tests__/**/?(*.)(test).js'],
         },
-    },
+        '<rootDir>/test',
+    ],
 };
--- test/jest.config.js
+++ test/jest.config.js
@@ -5,6 +5,7 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
 module.exports = {
+    displayName: 'integration',
     preset: '@lwc/jest-preset',
 
     moduleNameMapper: {

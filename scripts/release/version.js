ae12bf335c98206c9bf671c03d8ebf929f4730ad
Eugene Kashida <ekashida@gmail.com>
2021-04-08 03:34:18 -0700

    chore: version and publish scripts

---
+++ scripts/release/version.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+/*
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+const execa = require('execa');
+
+const ARGS = [
+    // https://github.com/lerna/lerna/tree/master/commands/version
+    'version',
+    // Use exact version number without using semver notation (i.e., ~ and ^)
+    '--exact',
+    // Skip lerna change detection and force version number updates across all packages
+    '--force-publish',
+    // Skip git commit and tag push
+    '--no-push',
+];
+
+const { stderr, stdin, stdout } = process;
+
+try {
+    execa.sync('lerna', ARGS, {
+        // Prioritize locally installed binaries (node_modules/.bin)
+        preferLocal: true,
+        stderr,
+        stdin,
+        stdout,
+    });
+} catch (ex) {
+    console.error(ex);
+    process.exit(1);
+}

7a6b481ee111f8a7e576d2abdf0cc7f5583e688a
Stephen Cook <cook.stephen@salesforce.com>
2021-06-21 12:53:15 -0700

    feat: add site/activeLanguages scoped module (@W-7088752)

--- packages/@lwc/jest-transformer/src/transforms/__tests__/site-scoped-import.test.js
+++ packages/@lwc/jest-transformer/src/transforms/__tests__/site-scoped-import.test.js
@@ -42,4 +42,100 @@ describe('@salesforce/site/Id import', () => {
         }
     `
     );
+
+    test(
+        'throws error if using named import',
+        `
+      import { id } from '@salesforce/site/Id';
+  `,
+        undefined,
+        'Invalid import from @salesforce/site/Id'
+    );
+
+    test(
+        'throws error if renamed default imports',
+        `
+      import { default as id } from '@salesforce/site/Id';
+  `,
+        undefined,
+        'Invalid import from @salesforce/site/Id'
+    );
+
+    test(
+        'throws error if renamed multiple default imports',
+        `
+      import { default as id, foo } from '@salesforce/site/Id';
+  `,
+        undefined,
+        'Invalid import from @salesforce/site/Id'
+    );
+});
+
+describe('@salesforce/site/activeLanguages import', () => {
+    test(
+        'does default transformation',
+        `
+    import activeLanguages from '@salesforce/site/activeLanguages';
+`,
+        `
+    let activeLanguages;
+
+    try {
+      activeLanguages = require("@salesforce/site/activeLanguages").default;
+    } catch (e) {
+      activeLanguages = [{
+        code: 'en-US',
+        label: 'English (US)'
+      }];
+    }
+`
+    );
+
+    test(
+        'allows non-@salesforce/site/activeLanguages named imports',
+        `
+    import { otherNamed } from './something-valid';
+    import activeLanguages from '@salesforce/site/activeLanguages';
+`,
+        `
+    import { otherNamed } from './something-valid';
+    let activeLanguages;
+
+    try {
+      activeLanguages = require("@salesforce/site/activeLanguages").default;
+    } catch (e) {
+      activeLanguages = [{
+        code: 'en-US',
+        label: 'English (US)'
+      }];
+    }
+`
+    );
+
+    test(
+        'throws error if using named import',
+        `
+    import { activeLanguages } from '@salesforce/site/activeLanguages';
+`,
+        undefined,
+        'Invalid import from @salesforce/site/activeLanguages'
+    );
+
+    test(
+        'throws error if renamed default imports',
+        `
+    import { default as activeLanguages } from '@salesforce/site/activeLanguages';
+`,
+        undefined,
+        'Invalid import from @salesforce/site/activeLanguages'
+    );
+
+    test(
+        'throws error if renamed multiple default imports',
+        `
+    import { default as activeLanguages, foo } from '@salesforce/site/activeLanguages';
+`,
+        undefined,
+        'Invalid import from @salesforce/site/activeLanguages'
+    );
 });

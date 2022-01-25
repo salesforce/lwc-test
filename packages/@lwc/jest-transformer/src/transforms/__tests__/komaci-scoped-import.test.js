b1a35dca429bd749c3c19e7e3e47bd79be9f63b0
Andrew Huffman <andrew.huffman@salesforce.com>
2022-01-24 22:55:26 -0600

    chore: add transformer for @salesforce/komaci

---
+++ packages/@lwc/jest-transformer/src/transforms/__tests__/komaci-scoped-import.test.js
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+const test = require('./utils/test-transform').test(require('../komaci-scoped-import'));
+
+describe('@salesforce/komaci import', () => {
+    test(
+        'does default transformation',
+        `
+        import komaciModule from '@salesforce/komaci/c__targetModule';
+    `,
+        `
+        let komaciModule;
+
+        try {
+          komaciModule = require("@salesforce/komaci/c__targetModule").default;
+        } catch (e) {
+          komaciModule = "c__targetModule";
+        }
+    `
+    );
+});

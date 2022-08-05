9365c71a5b485a44803e309b2402e1291ac445a7
Jose David Rodriguez Velasco <jodarove@gmail.com>
2022-08-05 14:58:06 -0400

    feat: provide a ssr-html text serializer

--- test/src/modules/ssr/basic/__tests__/basic.ssr-test.js
+++ test/src/modules/ssr/basic/__tests__/basic.ssr-test.js
@@ -7,12 +7,24 @@
 import { renderComponent } from 'lwc';
 import Basic from '../basic';
 
-it('renders a basic component', () => {
+it('renders a basic component and saves formatted snapshot', () => {
     const renderedComponent = renderComponent('resolver-basic', Basic, { msg: 'Hello world' });
 
-    expect(renderedComponent).toMatchInlineSnapshot(
-        `"<resolver-basic><template shadowroot=\\"open\\"><h1>Basic, Hello world</h1></template></resolver-basic>"`
-    );
+    expect(renderedComponent).toMatchSnapshot();
+});
+
+it('renders a basic component and saves inline formatted snapshot', () => {
+    const renderedComponent = renderComponent('resolver-basic', Basic, { msg: 'Hello world' });
+
+    expect(renderedComponent).toMatchInlineSnapshot(`
+        <resolver-basic>
+          <template shadowroot="open">
+            <h1>
+              Basic, Hello world
+            </h1>
+          </template>
+        </resolver-basic>
+    `);
 });
 
 it('runs on node environment', () => {

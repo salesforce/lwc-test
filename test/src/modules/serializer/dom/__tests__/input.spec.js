90919abb64914c1954e8701d559a4979567a5334
Nolan Lawson <nlawson@salesforce.com>
2022-08-30 16:59:29 -0700

    test: inline snapshot

--- test/src/modules/serializer/dom/__tests__/input.spec.js
+++ test/src/modules/serializer/dom/__tests__/input.spec.js
@@ -4,26 +4,39 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
-import semver from 'semver'
-import { version as jestVersion } from 'jest/package.json'
+import semver from 'semver';
+import { version as jestVersion } from 'jest/package.json';
 
-const jestMajorVersion = semver.major(jestVersion)
-
-// These snapshots differ between Jest <=28 and Jest 29+. It's not really feasible to maintain snapshots
-// for multiple Jest versions, since Jest will complain about any obsolete snapshots.
-// So we test only in Jest 29. For earlier versions of Jest, these are only tested in CI, and we can
-// ignore Jest's warnings in that case.
-if (jestMajorVersion >= 29) {
-    it.each([
-        ['undefined', undefined],
-        ['null', null],
-        ['empty object', {}],
-        ['without the nodeType property', { anotherKindOfProperty: 5 }],
-        ['with an element node type', document.createElement('div')],
-        ['with a text node type', document.createTextNode('')],
-        ['with a comment node type', document.createComment('')],
-        ['with another kind of node type', document.createAttribute('foo')],
-    ])('should return the expected state when we have %s', (useCase, input) => {
-        expect(input).toMatchSnapshot();
+describe('should return the expected state', () => {
+    it('undefined', () => {
+        expect(undefined).toMatchInlineSnapshot('undefined');
     });
-}
+    it('null', () => {
+        expect(null).toMatchInlineSnapshot('', `null`);
+    });
+    it('empty object', () => {
+        expect({}).toMatchInlineSnapshot('', `{}`);
+    });
+    it('without the nodeType property', () => {
+        expect({ anotherKindOfProperty: 5 }).toMatchInlineSnapshot(
+            '',
+            `
+            {
+              "anotherKindOfProperty": 5,
+            }
+        `
+        );
+    });
+    it('with an element node type', () => {
+        expect(document.createElement('div')).toMatchInlineSnapshot('', `<div />`);
+    });
+    it('with a text node type', () => {
+        expect(document.createTextNode('')).toMatchInlineSnapshot('');
+    });
+    it('with a comment node type', () => {
+        expect(document.createComment('')).toMatchInlineSnapshot('', `<!---->`);
+    });
+    it('with another kind of node type', () => {
+        expect(document.createAttribute('foo')).toMatchInlineSnapshot('', `Attr {}`);
+    });
+});

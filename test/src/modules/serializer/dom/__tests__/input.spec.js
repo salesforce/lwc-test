a1896cbb311130094130b12376b33b43f8fd2506
Nolan Lawson <nlawson@salesforce.com>
2022-08-30 16:47:15 -0700

    test: add snapshot test back in

---
+++ test/src/modules/serializer/dom/__tests__/input.spec.js
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+import semver from 'semver'
+import { version as jestVersion } from 'jest/package.json'
+
+const jestMajorVersion = semver.major(jestVersion)
+
+// These snapshots differ between Jest <=28 and Jest 29+. It's not really feasible to maintain snapshots
+// for multiple Jest versions, since Jets will complain about any obsolete snapshots.
+// So we test only in Jest 29. For earlier versions of Jest, these are only tested in CI, and we can
+// ignore Jest's warnings in that case.
+if (jestMajorVersion >= 29) {
+    it.each([
+        ['undefined', undefined],
+        ['null', null],
+        ['empty object', {}],
+        ['without the nodeType property', { anotherKindOfProperty: 5 }],
+        ['with an element node type', document.createElement('div')],
+        ['with a text node type', document.createTextNode('')],
+        ['with a comment node type', document.createComment('')],
+        ['with another kind of node type', document.createAttribute('foo')],
+    ])('should return the expected state when we have %s', (useCase, input) => {
+        expect(input).toMatchSnapshot();
+    });
+}

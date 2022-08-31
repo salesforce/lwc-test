71cad40ae099f93f43d7a3cc29efd9aa1f3873d5
Nolan Lawson <nlawson@salesforce.com>
2022-08-31 09:40:37 -0700

    fix: add support for jest 29 (#158)

--- test/src/modules/serializer/dom/__tests__/input.spec.js
+++ test/src/modules/serializer/dom/__tests__/input.spec.js
@@ -4,16 +4,59 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
+import semver from 'semver';
+import { version as jestVersion } from 'jest/package.json';
 
-it.each([
-    ['undefined', undefined],
-    ['null', null],
-    ['empty object', {}],
-    ['without the nodeType property', { anotherKindOfProperty: 5 }],
-    ['with an element node type', document.createElement('div')],
-    ['with a text node type', document.createTextNode('')],
-    ['with a comment node type', document.createComment('')],
-    ['with another kind of node type', document.createAttribute('foo')],
-])('should return the expected state when we have %s', (useCase, input) => {
-    expect(input).toMatchSnapshot();
+const jestMajorVersion = semver.major(jestVersion);
+
+describe('should return the expected state', () => {
+    it('undefined', () => {
+        const input = undefined;
+        expect(input).toMatchInlineSnapshot('undefined');
+    });
+    it('null', () => {
+        const input = null;
+        expect(input).toMatchInlineSnapshot(`null`);
+    });
+    it('empty object', () => {
+        const input = {};
+        if (jestMajorVersion >= 29) { // Jest changed its snapshot format in v29
+            expect(input).toMatchInlineSnapshot(`{}`);
+        } else {
+            expect(input).toMatchInlineSnapshot(`Object {}`);
+        }
+    });
+    it('without the nodeType property', () => {
+        const input = { anotherKindOfProperty: 5 };
+        if (jestMajorVersion >= 29) { // Jest changed its snapshot format in v29
+            expect(input).toMatchInlineSnapshot(`
+                            {
+                              "anotherKindOfProperty": 5,
+                            }
+                    `
+            );
+        } else {
+            expect(input).toMatchInlineSnapshot(`
+                Object {
+                  "anotherKindOfProperty": 5,
+                }
+            `);
+        }
+    });
+    it('with an element node type', () => {
+        const input = document.createElement('div');
+        expect(input).toMatchInlineSnapshot(`<div />`);
+    });
+    it('with a text node type', () => {
+        const input = document.createTextNode('');
+        expect(input).toMatchInlineSnapshot('');
+    });
+    it('with a comment node type', () => {
+        const input = document.createComment('');
+        expect(input).toMatchInlineSnapshot(`<!---->`);
+    });
+    it('with another kind of node type', () => {
+        const input = document.createAttribute('foo');
+        expect(input).toMatchInlineSnapshot(`Attr {}`);
+    });
 });

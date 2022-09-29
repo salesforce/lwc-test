680592a171d354a31ee272b06fd0fb474a7d5312
Mohammed Abdul Sattar <abdulsattarmohammed@salesforce.com>
2022-09-29 18:37:21 +0530

    test: add test for values not tracked without allowDeclareFields

--- test/src/modules/smoke/typescript/typescript.ts
+++ test/src/modules/smoke/typescript/typescript.ts
@@ -4,12 +4,18 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
 export default class TypeScript extends LightningElement {
     targetLanguage: string;
+    uninitialized: string;
 
     connectedCallback() {
         this.targetLanguage = 'TypeScript';
+    }
+
+    @api
+    initialize() {
+        this.uninitialized = 'initialized';
     }
 }

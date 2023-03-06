b9a294cecab5eada1061dc1eaf0d950548998a31
Ravi Jayaramappa <ravi.jayaramappa@salesforce.com>
2023-03-06 14:00:47 -0800

    chore: scoped slots is generally available and there is no need to set this flag @W-12134716 (#175)
    * chore: scoped slots is generally available and there is no need to set this flag
    
    * chore: update lwc dependencies to 2.40.0
    
    * fix: use public yarn registry

--- packages/@lwc/jest-transformer/src/index.js
+++ packages/@lwc/jest-transformer/src/index.js
@@ -103,7 +103,6 @@ module.exports = {
                 strictSpecifier: false,
             },
             scopedStyles: isKnownScopedCssFile(filePath),
-            enableScopedSlots: true,
             enableLwcSpread: true,
         });
 

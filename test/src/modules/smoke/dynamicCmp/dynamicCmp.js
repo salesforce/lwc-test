eb1873673a71a92f4ba91c8b18ca0451408d9e49
James Tu <jmsjtu@gmail.com>
2023-03-28 11:50:28 -0700

    feat: enable new dynamic components (#177)

---
+++ test/src/modules/smoke/dynamicCmp/dynamicCmp.js
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+import { LightningElement } from 'lwc';
+
+export default class extends LightningElement {
+    ctor = null;
+
+    connectedCallback() {
+        this.loadCtor();
+    }
+
+    async loadCtor() {
+        const moduleName = 'smoke/child';
+        const module = await import(moduleName);
+
+        this.ctor = module.default;
+    }
+}

4f873dc9a6bc9a5c376b425bdcae96654f95efc3
Nolan Lawson <nlawson@salesforce.com>
2023-08-18 10:31:51 -0700

    fix(ssr): do not serialize ZWJ characters (#205)
    BREAKING CHANGE: snapshots must be updated

---
+++ test/src/modules/ssr/emptyTextExpression/__tests__/emptyTextExpression.ssr-test.js
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+import { renderComponent } from 'lwc';
+import EmptyTextExpression from 'ssr/emptyTextExpression';
+
+it('renders a basic component with an empty text expression', () => {
+    const renderedComponent = renderComponent('ssr-empty-text-expression', EmptyTextExpression, {});
+
+    expect(renderedComponent).toMatchSnapshot();
+});

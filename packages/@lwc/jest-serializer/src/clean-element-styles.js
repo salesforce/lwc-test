38716e033a8b170ee437188978f842aa56a0a2eb
James Tu <j.tu@salesforce.com>
2024-04-04 14:54:13 -0700

    chore: update based on feedback

--- packages/@lwc/jest-serializer/src/clean-element-styles.js
+++ packages/@lwc/jest-serializer/src/clean-element-styles.js
@@ -15,9 +15,9 @@ function cleanElementStyles(elm) {
         // Replace multiple space, tab, line break with single space
         .replace(/[\s\n\t]+/g, ' ')
         .split(';')
-        // Preserve space character following a ';', style="color: blue; text-align: center"
-        .map((attr) => attr.trimEnd())
-        .join(';');
+        .map((_) => `${_.trim()};`)
+        .filter((_) => _ !== ';')
+        .join(' ');
     if (normalizedStyleAttribute) {
         elm.setAttribute('style', normalizedStyleAttribute);
     } else {

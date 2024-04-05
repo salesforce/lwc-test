017d3ffad53d668ce47d6acfe5af82429afcacac
Nolan Lawson <nlawson@salesforce.com>
2024-04-05 15:02:05 -0700

    refactor: use Array.from

--- packages/@lwc/jest-serializer/src/clean-element-styles.js
+++ packages/@lwc/jest-serializer/src/clean-element-styles.js
@@ -8,13 +8,11 @@
 function cleanElementStyles(elm) {
     // Normalize trivial whitespace differences (e.g. `style="  foo   bar "` becomes `style="foo bar"`
     // and `style=""` is just removed).
-    const styles = [];
-    for (let i = 0; i < elm.style.length; i++) {
-        const prop = elm.style[i];
+    const styles = Array.from(elm.style).map((prop) => {
         const value = elm.style.getPropertyValue(prop);
         const priority = elm.style.getPropertyPriority(prop);
-        styles.push(`${prop}: ${value}${priority === 'important' ? ' !important' : ''};`);
-    }
+        return `${prop}: ${value}${priority === 'important' ? ' !important' : ''};`;
+    });
 
     if (styles.length) {
         elm.setAttribute('style', styles.join(' '));

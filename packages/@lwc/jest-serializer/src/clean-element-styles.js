adb8770231458aebf26b179e7c82176572a50a99
Nolan Lawson <nlawson@salesforce.com>
2024-04-08 09:49:47 -0700

    fix: use jsdom to parse style attr and serialize it (#239)

--- packages/@lwc/jest-serializer/src/clean-element-styles.js
+++ packages/@lwc/jest-serializer/src/clean-element-styles.js
@@ -8,26 +8,14 @@
 function cleanElementStyles(elm) {
     // Normalize trivial whitespace differences (e.g. `style="  foo   bar "` becomes `style="foo bar"`
     // and `style=""` is just removed).
-    const styleAttribute = elm.getAttribute('style') || '';
-    const normalizedStyleAttribute = styleAttribute
-        // Remove leading/trailing space
-        .trim()
-        // Replace multiple space, tab, line break with single space
-        .replace(/\s+/g, ' ')
-        .split(';')
-        .map((_) => `${_.trim()};`)
-        .filter((_) => _ !== ';')
-        // Normalize whitespace around colons, e.g. `color :  red` -> `color: red`
-        .map((declaration) =>
-            declaration
-                .split(':')
-                // remove whitespace before `important`, e.g. `! important` -> `!important`
-                .map((_) => _.trim().replace(/!\simportant/, '!important'))
-                .join(': '),
-        )
-        .join(' ');
-    if (normalizedStyleAttribute) {
-        elm.setAttribute('style', normalizedStyleAttribute);
+    const styles = Array.from(elm.style).map((prop) => {
+        const value = elm.style.getPropertyValue(prop);
+        const priority = elm.style.getPropertyPriority(prop);
+        return `${prop}: ${value}${priority === 'important' ? ' !important' : ''};`;
+    });
+
+    if (styles.length) {
+        elm.setAttribute('style', styles.join(' '));
     } else {
         elm.removeAttribute('style');
     }
